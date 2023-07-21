177ac7b8f5679ae04e2ec591aa7f9ecfd8b10e12
JaraSanchezLuis <107270563+luisenjs@users.noreply.github.com>
2023-07-20 21:19:18 -0500

    Update Tienda.java

--- TallerPatrones/src/main/java/ChainofResponsability/Tienda.java
+++ TallerPatrones/src/main/java/ChainofResponsability/Tienda.java
@@ -1,5 +1,17 @@
 package ChainofResponsability;
 
 public class Tienda {
+
+    public static void main(String[] args){
+        Department d1 = new CustomerServiceDepartment();
+        Department d2 = new TechnicalDepartment();
+        Department d3 = new Inventory();
+        Department d4 = new Management();
+        Articulo articulo = new Articulo();
+        d1.setNext(d2);
+        d2.setNext(d3);
+        d3.setNext(d4);
+        d1.consultar(articulo);
+    }
     
 }
